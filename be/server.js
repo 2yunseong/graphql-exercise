75c5445ee7d159d75155afadbdbb23317205309a
2yunseong <dbsdltjd3701@naver.com>
2022-06-28 23:27:32 +0900

    feat : mutation 실습

--- be/server.js
+++ be/server.js
@@ -15,6 +15,11 @@ const typeDefs = gql`
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
+  }
 `;
 const server = new ApolloServer({ typeDefs });
 
