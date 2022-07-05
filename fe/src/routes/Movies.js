8ce6d3e44f8c46b327aa308c7c20591c2dea3d2a
2yunseong <dbsdltjd3701@naver.com>
2022-07-05 11:40:57 +0900

    feat : AplloProvider 추가

---
+++ fe/src/routes/Movies.js
@@ -0,0 +1,12 @@
+import { gql, useApolloClient, useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+
+const g
+const Movies = () => {
+  useQuery()
+
+
+  return <div>hi</div>;
+};
+
+export default Movies;
